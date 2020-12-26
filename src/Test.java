73427679da91054bd51a0d780e451873e15b302f
day1 <921936338@qq.com>
2020-12-26 12:13:10 +0800

    1、User创建 2、密码对比

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args){
+        /*
+        读文件
+         */
+        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\CmdshopA\\src\\user.xlsx");
+        ReadExcel readexcel=new ReadExcel();//创建对象
+        User users[]=readexcel.readExcel(file);
+        System.out.println("请输入用户名");
+        Scanner sc=new Scanner(System.in);
+        String username=sc.next();
+
+        System.out.println("请输入密码");
+        String pasword=sc.next();
+        for(User user:users){
+
+        }
+    }
+
+
 }
---
+++ src/User.java
@@ -0,0 +1,6 @@
+public class User {
+    String username;//用户名
+    String password;//密码
+    String phone;//电话
+    String adress;//地址
+}
